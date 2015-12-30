2c98801de32cd74ed58e55d704287bed95c43900
Andrew Hampe <andrew@faunadb.com>
2015-12-29 21:36:38 -0600

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,47 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from codecs import open
+from os import path
+from faunadb import __version__ as version
+
+# Load the README file for use in the long description
+local_dir = path.abspath(path.dirname(__file__))
+with open(path.join(local_dir, "README.rst"), encoding="utf-8") as f:
+  long_description = f.read()
+
+requires = [
+  "iso8601",
+  "requests"
+]
+
+extra_requires = {
+  "docs": ["pynt", "sphinx", "sphinx_rtd_theme"],
+  "test": ["pynt", "nose", "pylint", "testfixtures"]
+}
 
 setup(
   name="faunadb",
+  version=version,
   description="FaunaDB Python client",
+  long_description=long_description,
+  url="https://github.com/faunadb/faunadb-python",
   author="FaunaDB",
   author_email="priority@faunadb.com",
-  url="github.com/faunadb/faunadb-python",
-  version=0,
-  install_requires=["iso8601", "requests"],
-  tests_require=["nose", "pylint", "pynt", "sphinx", "sphinx_rtd_theme", "testfixtures"],
+  license="MPL 2.0",
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+    "Programming Language :: Python :: 2 :: Only",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Database",
+    "Topic :: Database :: Front-Ends",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: Unix",
+  ],
+  keywords="faunadb fauna",
   packages=["faunadb"],
-  scripts=[],
-  license="MPL 2.0")
+  install_requires=requires,
+  extra_requires=extra_requires
+)
