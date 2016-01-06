fe9f27b9c1e3fb209d781c262237a97f8f8c44eb
Andrew Hampe <andrew@faunadb.com>
2016-01-06 11:21:56 -0600

    Fix path addition for docs generation

--- docs/conf.py
+++ docs/conf.py
@@ -14,12 +14,11 @@
 
 import sys
 import os
-import shlex
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.join(os.path.abspath('.'), '../faunadb'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
