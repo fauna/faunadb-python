e474105d80a5fcd16401d4e71f3979a664f7c115
andy-faunadb <andy@faunadb.com>
2016-01-11 11:39:59 -0800

    Use assert_raises when capturing exceptions

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,16 +1,15 @@
 from requests import codes
 
 from faunadb import query
-from faunadb.errors import ErrorData, Failure, ValidationFailed, FaunaError, BadRequest, \
-  InternalError, MethodNotAllowed, NotFound, PermissionDenied, Unauthorized, UnavailableError, \
-  InvalidResponse
+from faunadb.errors import ErrorData, Failure, ValidationFailed, BadRequest, InternalError, \
+  MethodNotAllowed, NotFound, PermissionDenied, Unauthorized, UnavailableError, InvalidResponse
 from faunadb.objects import Ref
 
 from tests.helpers import FaunaTestCase, mock_client
 
 class ErrorsTest(FaunaTestCase):
   def test_request_result(self):
-    err = capture_exception(lambda: self.client.query({"foo": "bar"}))
+    err = self.assert_raises(BadRequest, lambda: self.client.query({"foo": "bar"}))
     assert err.request_result.request_content == {"foo": "bar"}
 
   def test_invalid_response(self):
@@ -29,7 +28,7 @@ class ErrorsTest(FaunaTestCase):
 
   def test_unauthorized(self):
     client = self.get_client(secret="bad_key")
-    assert_http_error(
+    self.assert_http_error(
       lambda: client.query(query.get(self.db_ref)), Unauthorized, "unauthorized")
 
   def test_permission_denied(self):
@@ -39,15 +38,16 @@ class ErrorsTest(FaunaTestCase):
         query.create(Ref("keys"), query.object(database=self.db_ref, role="client")))["secret"]
     )
 
-    exception = capture_exception(lambda: client.query(query.paginate(Ref("databases"))))
-    assert isinstance(exception, PermissionDenied)
+    exception = self.assert_raises(
+      PermissionDenied,
+      lambda: client.query(query.paginate(Ref("databases"))))
     assert_error(exception, "permission denied", ["paginate"])
 
   def test_not_found(self):
-    assert_http_error(lambda: self.client.get("classes/not_found"), NotFound, "not found")
+    self.assert_http_error(lambda: self.client.get("classes/not_found"), NotFound, "not found")
 
   def test_method_not_allowed(self):
-    assert_http_error(
+    self.assert_http_error(
       lambda: self.client.delete("classes"), MethodNotAllowed, "method not allowed")
 
   def test_internal_error(self):
@@ -55,37 +55,40 @@ class ErrorsTest(FaunaTestCase):
     code_client = mock_client(
       '{"errors": [{"code": "internal server error", "description": "sample text", "stacktrace": []}]}',
       codes.internal_server_error)
-    assert_http_error(lambda: code_client.get("error"), InternalError, "internal server error")
+    self.assert_http_error(lambda: code_client.get("error"), InternalError, "internal server error")
 
   def test_unavailable_error(self):
     client = mock_client(
       '{"errors": [{"code": "unavailable", "description": "on vacation"}]}',
       codes.unavailable)
-    assert_http_error(lambda: client.get(''), UnavailableError, "unavailable")
+    self.assert_http_error(lambda: client.get(''), UnavailableError, "unavailable")
   #endregion
 
   #region ErrorData
   def test_invalid_expression(self):
-    self._assert_query_error({"foo": "bar"}, "invalid expression")
+    self._assert_query_error({"foo": "bar"}, BadRequest, "invalid expression")
 
   def test_unbound_variable(self):
-    self._assert_query_error({"var": "x"}, "unbound variable")
+    self._assert_query_error({"var": "x"}, BadRequest, "unbound variable")
 
   def test_invalid_argument(self):
-    self._assert_query_error({"add": [1, "two"]}, "invalid argument", ["add", 1])
+    self._assert_query_error({"add": [1, "two"]}, BadRequest, "invalid argument", ["add", 1])
 
   def test_instance_not_found(self):
     # Must be a reference to a real class or else we get InvalidExpression
     self.client.post("classes", {"name": "foofaws"})
-    self._assert_query_error({"get": {"@ref": "classes/foofaws/123"}}, "instance not found")
+    self._assert_query_error(
+      {"get": {"@ref": "classes/foofaws/123"}},
+      NotFound,
+      "instance not found")
 
   def test_value_not_found(self):
-    self._assert_query_error({"select": ["a"], "from": {"object": {}}}, "value not found")
+    self._assert_query_error({"select": ["a"], "from": {"object": {}}}, NotFound, "value not found")
 
   def test_instance_already_exists(self):
     self.client.post("classes", {"name": "duplicates"})
     ref = self.client.post("classes/duplicates", {})["ref"]
-    self._assert_query_error({"create": ref}, "instance already exists", ["create"])
+    self._assert_query_error({"create": ref}, BadRequest, "instance already exists", ["create"])
   #endregion
 
   #region InvalidData
@@ -109,7 +112,7 @@ class ErrorsTest(FaunaTestCase):
       "classes/gerbils", {"data": {"x": 1}}, "duplicate value", ["data", "x"])
 
   def _assert_invalid_data(self, class_name, data, code, field):
-    exception = capture_exception(lambda: self.client.post(class_name, data))
+    exception = self.assert_raises(BadRequest, lambda: self.client.post(class_name, data))
     assert_error(exception, "validation failed", [])
     failures = exception.errors[0].failures
     assert len(failures) == 1
@@ -126,22 +129,12 @@ class ErrorsTest(FaunaTestCase):
     vf = ValidationFailed("vf_desc", ["vf"], [failure])
     assert repr(vf) == "ValidationFailed('vf_desc', ['vf'], [Failure('code', 'desc', ['a', 'b'])])"
 
-  def _assert_query_error(self, q, code, position=None):
+  def _assert_query_error(self, q, exception_class, code, position=None):
     position = position or []
-    assert_error(capture_exception(lambda: self.client.query(q)), code, position)
+    assert_error(self.assert_raises(exception_class, lambda: self.client.query(q)), code, position)
 
-
-def capture_exception(func):
-  try:
-    func()
-  except FaunaError as exception:
-    return exception
-
-
-def assert_http_error(func, exception_cls, code):
-  exception = capture_exception(func)
-  assert isinstance(exception, exception_cls)
-  assert_error(exception, code)
+  def assert_http_error(self, action, exception_cls, code):
+    assert_error(self.assert_raises(exception_cls, action), code)
 
 
 def assert_error(exception, code, position=None):
