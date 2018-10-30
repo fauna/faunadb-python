38991acd01956381906b95d25825872bc2177811
Eduardo V. Bruno <eduardo.bruno@fauna.com>
2018-10-30 11:53:49 -0300

    Expose last txn time

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,18 +5,29 @@ from tests.helpers import FaunaTestCase
 class ClientTest(FaunaTestCase):
 
   def test_ping(self):
-    old_time = self.client.last_txn
+    old_time = self.client.get_last_txn_time()
     self.assertEqual(self.client.ping("node"), "Scope node is OK")
-    new_time = self.client.last_txn
+    new_time = self.client.get_last_txn_time()
 
     self.assertEqual(old_time, new_time) # client.ping should not update last-txn-time
 
   def test_last_txn_time(self):
-    old_time = self.client.last_txn
+    old_time = self.client.get_last_txn_time()
     self.client.query({})
-    new_time = self.client.last_txn
+    new_time = self.client.get_last_txn_time()
 
     self.assertTrue(old_time < new_time) # client.query should update last-txn-time
+
+  def test_last_txn_time_upated(self):
+    first_seen = self.client.get_last_txn_time()
+
+    new_time = first_seen - 12000
+    self.client.sync_last_txn_time(new_time)
+    self.assertEqual(self.client.get_last_txn_time(), first_seen) # last-txn can not be smaller
+
+    new_time = first_seen + 12000
+    self.client.sync_last_txn_time(new_time)
+    self.assertEqual(self.client.get_last_txn_time(), new_time) # last-txn can move forward
 
   def test_error_on_closed_client(self):
     client = FaunaClient(secret="secret")
