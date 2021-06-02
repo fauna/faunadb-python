488840c4f1f6d03eee21fe664ecee2db3966749b
SZinkevych <s.zinkevych@gmail.com>
2021-06-02 11:16:30 +0300

    change approach

--- faunadb/__init__.py
+++ faunadb/__init__.py
@@ -10,16 +10,11 @@ __copyright__ = "2020 Fauna, Inc"
 
 response = requests.get('https://pypi.org/pypi/faunadb/json')
 latest_version = response.json().get('info').get('version')
-latest_version_arr = latest_version.split(".")
-current_version = __version__.split(".")
-is_new_version_available = False
-
-for i in range(len(current_version)):
-  if latest_version_arr[i] > current_version[i]:
-    is_new_version_available = True
 
 
-if True: #is_new_version_available:
+
+
+if latest_version > __version__:
   msg1 = "New fauna version available {} => {}".format(__version__, latest_version)
   msg2 = "Changelog: https://github.com/fauna/faunadb-python/blob/master/CHANGELOG.md"
   width = 80
