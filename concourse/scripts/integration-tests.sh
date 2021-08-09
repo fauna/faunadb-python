e95cee9909db3de5007a96c1e83cc846e82cc5d8
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2021-08-09 17:13:14 +0300

    Release v4.1.1

--- concourse/scripts/integration-tests.sh
+++ concourse/scripts/integration-tests.sh
@@ -4,7 +4,11 @@ set -eou
 
 apk add --update make
 
-pip install .
-pip install nose2
-pip install pylint
-make test
+#pip install .
+#pip install nose2
+#pip install pylint
+#pip install requests
+#make test
+
+pip install codecov
+coverage run setup.py test
