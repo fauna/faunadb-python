943c604df7f7b57d9c3a1a253fed5f87a1dcffec
Henry Ball <henry.ball@fauna.com>
2022-09-15 10:51:04 -0700

    add logging to debug pipeline failure

--- tests/helpers.py
+++ tests/helpers.py
@@ -78,6 +78,7 @@ class FaunaTestCase(TestCase):
 
   @classmethod
   def _get_client(cls):
+    print("Initializing client with: " + _FAUNA_DOMAIN + ", " + _FAUNA_SCHEME + ", " + _FAUNA_PORT)
     args = {
       "domain": _FAUNA_DOMAIN,
       "scheme": _FAUNA_SCHEME,
